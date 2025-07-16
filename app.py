61aabef668bbeea63fa287f7c562f8da3939d0bf
pedrof926 <piurosfernandes@gmail.com>
2025-07-16 08:02:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, html, dcc
 
 app = Dash(__name__)
-server = app.server
+server = app.server  # ESSENCIAL para Render + Gunicorn
 
 app.layout = html.Div([
-    html.H1("Exemplo Dash 3.x"),
-    dcc.Markdown("Tudo funcionando!")
+    html.H1("Teste Render - Dash OK!"),
+    dcc.Markdown("Se você vê isso, tá tudo certo!")
 ])
 
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 8050))
     app.run_server(host='0.0.0.0', port=port, debug=True)
+
